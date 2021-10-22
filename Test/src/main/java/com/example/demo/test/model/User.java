42ad865ddf5fffb679611d366a3a85bf92c9ce54
KH54 <pkh543@naver.com>
2021-10-22 17:12:16 +0900

    유저 객체

--- Test/src/main/java/com/example/demo/test/model/User.java
+++ Test/src/main/java/com/example/demo/test/model/User.java
@@ -1,114 +1,190 @@
 package com.example.demo.test.model;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import org.springframework.validation.annotation.Validated;
+
 /**
  * 
- * @description : User 객체의 생성자 
+ * @description : User 객체의 생성자
  *
- * @author : Kyunghun Park 
+ * @author : Kyunghun Park
  * @since : 2021. 10. 18. 오전 11:05:39
  *
  */
+@Validated
 public class User {
-    // 유저 ID -- PK   
-    private String userId;
+    // 유저 ID -- PK
+    @NotBlank(message="plz insert value")
+    @Size(max=8)
+    private String id;
     // 유저 PW
-    private String userPwd;
+    @NotBlank
+    @Size(max=16)
+    private String pwd;
     // 유저 닉네임
-    private String userNick;
+    @NotBlank
+    @Size(max=8)
+    private String name;
+    @NotBlank
+    @Size(max=8)
+    private String gender;
+    @Positive
+    private int age;
 
     public User() {
 
     }
 
-    // 생성자 오버로딩 
-    public User(String userId, String userPwd, String userNick) {
-        this.userId = userId;
-        this.userPwd = userPwd;
-        this.userNick = userNick;
+    // 생성자 오버로딩
+    public User(String id, String pwd, String name, String gender, int age) {
+        this.id = id;
+        this.pwd = pwd;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
     }
+    
+   
 
     /**
-     * userId을(를) 가져옵니다.
+     * id을(를) 가져옵니다.
      *
      * @author "KyungHun Park"
      * @since 2021. 10. 12. 오후 1:18:48
-     * @return userId
+     * @return id
      */
-    public String getUserId() {
-        return userId;
+    public String getId() {
+        return id;
     }
 
     /**
-     * userId을(를) 설정합니다.
+     * id을(를) 설정합니다.
      *
      * @author "KyungHun Park"
      * @since 2021. 10. 12. 오후 1:18:48
-     * @param userId
+     * @param id
      */
-    public void setUserId(String userId) {
-        this.userId = userId;
+    public void setId(String id) {
+        this.id = id;
     }
 
     /**
-     * userPwd을(를) 가져옵니다.
+     * pwd을(를) 가져옵니다.
      *
      * @author "KyungHun Park"
      * @since 2021. 10. 12. 오후 1:18:48
-     * @return userPwd
+     * @return pwd
      */
-    public String getUserPwd() {
-        return userPwd;
+    public String getPwd() {
+        return pwd;
     }
 
     /**
-     * userPwd을(를) 설정합니다.
+     * pwd을(를) 설정합니다.
      *
      * @author "KyungHun Park"
      * @since 2021. 10. 12. 오후 1:18:48
-     * @param userPwd
+     * @param pwd
      */
-    public void setUserPwd(String userPwd) {
-        this.userPwd = userPwd;
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
     }
 
     /**
-     * userNick을(를) 가져옵니다.
+     * name을(를) 가져옵니다.
      *
      * @author "KyungHun Park"
      * @since 2021. 10. 12. 오후 1:18:48
-     * @return userNick
+     * @return name
      */
-    public String getUserNick() {
-        return userNick;
+    public String getName() {
+        return name;
     }
 
     /**
-     * userNick을(를) 설정합니다.
+     * name을(를) 설정합니다.
      *
      * @author "KyungHun Park"
      * @since 2021. 10. 12. 오후 1:18:48
-     * @param userNick
+     * @param name
      */
-    public void setUserNick(String userNick) {
-        this.userNick = userNick;
+    public void setName(String name) {
+        this.name = name;
     }
 
     /**
-     *
+     * gender를 가져옵니다.
+     * 
      * @author "KyungHun Park"
-     * @since 2021. 10. 13. 오후 1:48:59
+     * @since 2021. 10. 18.
+     *
+     * @return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * 
+     * geder를 설정합니다.
+     * 
+     * @author "KyungHun Park"
+     * @since 2021. 10. 18.
+     *
+     * @param gender :
+     *
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * age를 호출합니다.
+     * 
+     * @author "KyungHun Park"
+     * @since 2021. 10. 18.
+     *
+     *
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * age를 설정합니다.
+     * 
+     * @author "KyungHun Park"
+     * @since 2021. 10. 18.
+     *
+     * @param age : 나이
+     *
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @see java.lang.Object#toString()
      */
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
-        builder.append("[userId=");
-        builder.append(userId);
-        builder.append(", userPwd=");
-        builder.append(userPwd);
-        builder.append(", userNick=");
-        builder.append(userNick);
+        builder.append("User [id=");
+        builder.append(id);
+        builder.append(", pwd=");
+        builder.append(pwd);
+        builder.append(", name=");
+        builder.append(name);
+        builder.append(", gender=");
+        builder.append(gender);
+        builder.append(", age=");
+        builder.append(age);
         builder.append("]");
         return builder.toString();
     }
+
 
 }
