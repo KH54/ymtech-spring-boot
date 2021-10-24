95b7878c2a50ec61bf8686c7086776e55dc479dc
KH54 <pkh543@naver.com>
2021-10-24 22:05:34 +0900

    유저 부분 조회 Response

---
+++ Test/src/main/java/com/example/test/controller/model/user/ElementRes.java
@@ -0,0 +1,45 @@
+package com.example.test.controller.model.user;
+
+import com.example.test.model.User;
+
+/**
+ * 부분 조회 정보를 담기 위한 Response 클래스
+ *
+ * @author "KyungHun Park"
+ * @since 2021. 10. 24. 오후 9:55:28
+ *
+ * @modified 2021. 10. 24. 오후 9:55:28 || Kyunghun Park || 최초 생성
+ *
+ */
+public class ElementRes {
+
+    private User element;
+
+    // 기본 생성자
+    public ElementRes() {
+
+    }
+
+    /**
+     * element을(를) 가져옵니다.
+     *
+     * @author "KyungHun Park"
+     * @since 2021. 10. 15. 오후 3:46:51
+     * @return element
+     */
+    public User getElement() {
+        return element;
+    }
+
+    /**
+     * element을(를) 설정합니다.
+     *
+     * @author "KyungHun Park"
+     * @since 2021. 10. 15. 오후 3:46:51
+     * @param element
+     */
+    public void setElement(User element) {
+        this.element = element;
+    }
+
+}
