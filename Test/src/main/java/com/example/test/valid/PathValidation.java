fc37fc99bb27a79d721627b5eb0e7b0d8fecba45
KH54 <pkh543@naver.com>
2021-10-24 22:09:08 +0900

    대기

---
+++ Test/src/main/java/com/example/test/valid/PathValidation.java
@@ -0,0 +1,8 @@
+package com.example.test.valid;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PathValidation {
+    StringBuilder failedHistory = new StringBuilder();
+}
