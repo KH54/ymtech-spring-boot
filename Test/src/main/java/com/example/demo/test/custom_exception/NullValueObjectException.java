f9b6023e0a78d591cc4730945fad1322f7b61dc6
KH54 <pkh543@naver.com>
2021-10-24 22:04:41 +0900

    /

--- Test/src/main/java/com/example/demo/test/custom_exception/NullValueObjectException.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.test.custom_exception;
-
-public class  NullValueObjectException extends RuntimeException {
-    // 컴파일러가 계산한 값을 부여하지 않고 일정한 값을 지정해 주기 위한 UID
-    private static final long serialVersionUID = 1L;
-}
--- Test/src/main/java/com/example/demo/test/valid/PathValidation.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.test.valid;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class PathValidation {
-    StringBuilder failedHistory = new StringBuilder();
-}
